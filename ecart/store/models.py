1e69b56dd69fb7e295b9ef17d7796c67ae4fc56d
virinchimanepalli <virinchi.msk83@gmail.com>
2020-07-07 22:22:09 +0530

    rendering data

--- ecart/store/models.py
+++ ecart/store/models.py
@@ -14,11 +14,22 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
 
-class order(models.Model):
+    @property
+
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
+class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank = True, null =True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False,null=True,blank=False)
@@ -26,4 +37,30 @@ class order(models.Model):
 
     def __str__(self):
         return str(self.id)
-        
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL, blank = True, null =True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null= True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now=True)
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank = True, null= True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null= True)
+    address =  models.CharField(max_length=200, null=True)
+    city =  models.CharField(max_length=200, null=True)
+    state =  models.CharField(max_length=200, null=True)
+    zipcode =  models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.address
+
+
+
+
+
+
+
+
+
