5c6f397920d77bfe8553d8100e12e4d795ceb667
virinchimanepalli <virinchi.msk83@gmail.com>
2020-07-11 20:57:47 +0530

    add to cart

--- ecart/store/models.py
+++ ecart/store/models.py
@@ -21,7 +21,6 @@ class Product(models.Model):
         return self.name
 
     @property
-
     def imageURL(self):
         try:
             url = self.image.url
@@ -38,11 +37,39 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital ==False:
+                shipping = True
+        return shipping
+
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum(item.quantity for item in orderitems)
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL, blank = True, null =True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null= True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now=True)
+
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank = True, null= True)
