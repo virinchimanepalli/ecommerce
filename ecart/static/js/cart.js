d6644061c20f6d3fe11ea59a98b7f8c4054b15aa
virinchimanepalli <virinchi.msk83@gmail.com>
2020-08-06 23:36:15 +0530

    guest-checkIn

--- ecart/static/js/cart.js
+++ ecart/static/js/cart.js
@@ -1,21 +1,19 @@
 var updateBtns = document.getElementsByClassName('update-cart')
-// feteching and promise concepts have to be scene again!!!see csrf documentation
-for(var i =0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click',function(){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId,'action:', action)
 
-        console.log('user:',user)
-        if(user == 'AnonymousUser'){
-            console.log("not logged in")}
-        else{
-            updateUserOrder(productId,action)
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
 
-        }
-        })
-}
-
+		if (user == 'AnonymousUser'){
+			addCookieItem(productId, action)
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
+} 
 
 function updateUserOrder(productId, action){
 	console.log('User is authenticated, sending data...')
@@ -36,4 +34,30 @@ function updateUserOrder(productId, action){
 		.then((data) => {
 		    location.reload()
 		});
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	location.reload()
 }
